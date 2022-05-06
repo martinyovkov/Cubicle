4109b40410657a4fd0b29dc52835f9de6da6d6e3
martinyovkov <martiyovkov10@gmail.com>
2022-05-06 15:12:39 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,8 @@ const router = express.Router();
 router.use(aboutController);
 router.use(homeController);
 router.use('/cube', cubeController);
+
+
 router.use((req, res)=> {
     res.render('404');
 });
