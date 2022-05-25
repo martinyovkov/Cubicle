3e172d4510992fe4d805405f43680116fb060526
martinyovkov <martiyovkov10@gmail.com>
2022-05-25 18:39:50 +0300

    repairs

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -15,6 +15,27 @@ class Cube{
             description: 'Standart rubic cube',
             imageUrl: 'https://classteaching.files.wordpress.com/2019/09/rubiks-cube.jpg',
             difficulty: '3'
+          },
+          {
+            id: 'qpfp73asl3lqlrqh',
+            name: 'sad cube ',
+            description: 'dsdasad',
+            imageUrl: 'https://static.wikia.nocookie.net/justshapesandbeats/images/b/b8/Friend.png/revision/latest?cb=20181104123324',
+            difficulty: '4'
+          },
+          {
+            id: 'qpfp73asl3lqn5rr',
+            name: 'Pyramid cube',
+            description: 'asaaad',
+            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61cJE0vAo1L._AC_SL1200_.jpg',
+            difficulty: '5'
+          },
+          {
+            id: 'qpfp73asl3lqnshy',
+            name: 'Cute cube',
+            description: 'asd',
+            imageUrl: 'https://i.pinimg.com/originals/4d/43/48/4d4348b7d26e182533d278d3d6e4f29f.jpg',
+            difficulty: '1'
           }
     ]
 
