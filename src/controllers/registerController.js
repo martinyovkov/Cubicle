47dea13762a62c708904dac35448daa8d30bcc73
martinyovkov <martiyovkov10@gmail.com>
2022-09-30 16:57:20 +0300

    add express validator

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const {body, validationResult} = require('express-validator');
 const authService = require('../services/authService');
 const router = express.Router();
 
 const renderRegister = (req,res)=>{
     res.render('registerPage');
 }
-const registerUser = async (req,res)=>{;
+const registerUser = async (req,res)=>{
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors:errors.array() });
+    }
+
    let createdUser = await authService.register(req.body);
 
    if (createdUser) {
@@ -18,5 +25,5 @@ const registerUser = async (req,res)=>{;
 }
 
 router.get('/register', renderRegister);
-router.post('/register', registerUser);
+router.post('/register', body('username').not().isEmail() ,registerUser);
 module.exports = router;
